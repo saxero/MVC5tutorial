93ef2be543f34079e8935832f4b593e1e8bfa8ef
Josep Vives <josep.vives@clindemiurg.com>
2016-10-31 15:47:45 +0100

    first commit

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(getCustomers());
+            
+        }
+
+        public ActionResult Details(int? Id)
+        {
+            if (Id==null)
+                return HttpNotFound();
+
+            var Customer = getCustomers().SingleOrDefault(c => c.Id == Id);
+            if (Customer == null)
+                return HttpNotFound();
+            else
+                return View(Customer);
+        }
+
+        IEnumerable<Customer> getCustomers()
+        {
+            return new List<Customer>{
+                new Customer{ Id=1, Name="Johh Smith"},
+                new Customer{ Id=2, Name="Mary Williams"}
+            };
+            //return new List<Customer>();
+        }
+
+        
+
+    }
+}
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = getMovies();
+            
+            
+            return View(movies);
+        }
+
+        public ActionResult Details(int? id){
+            if (id == null)
+                return HttpNotFound();
+
+            var movie = getMovies().SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        List<Movie> getMovies()
+        {
+            return new List<Movie>{
+                new Movie(){Id=1, Name="Shrek"},
+                new Movie(){Id=2, Name="Wall-e"}
+            };
+        }
+    }
+}
